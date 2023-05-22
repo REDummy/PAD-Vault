a1f00ee582d8e89f7d02491d51e05c66858051f8
REDummy <215314181@student.usd.ac.id>
2023-05-22 10:42:47 +0700

    added more plot

--- Pandas/USIP2/temp.py
+++ Pandas/USIP2/temp.py
@@ -1,54 +1,85 @@
 import pandas as pd
 import numpy as np
+import seaborn
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("winequality-red.csv", sep=";")
+pd.set_option('display.width', 400)
+pd.set_option('display.max_columns', 15)
 
-# Basic statistics
-statistics = df.describe()
-print("\nStatistics:")
-print(statistics)
+df = pd.read_csv("winequality-white.csv", sep=";")
 
-# # Correlation matrix
-# correlation = df.corr(method='kendall')
-# print("\nCorrelation Matrix:")
-# print(correlation)
+print(df.describe())
 
-# # kde density that doesnt make that much sense
-# df['alcohol'].plot.kde()
-# plt.show()
-
-# # semi 3d mapping between 3 values
-# df.plot.scatter(x='pH',y='alcohol',c='quality',cmap='coolwarm')
-# plt.show()
-
-# # testing out sorting
-# df.sort_values("quality", axis = 0, ascending = False,
-#                  inplace = True, na_position ='last')
-
-# print(df.head())
+#plot - pair plot
+seaborn.pairplot(df)
+plt.show()
+seaborn.lineplot(x="citric acid", y="quality", data=df)
+plt.show()
 
 
+# Console - Correlation matrix
+correlation = df.corr(method='kendall')
+print("\nCorrelation Matrix:")
+print(correlation)
 
-# Plot is wayy to broad and can't be seen nicely
-# plt.style.use('ggplot')
-# df.plot.area(alpha=0.5)
-# plt.show()
+# plot - kde density
+df['alcohol'].plot.kde()
+plt.show()
 
-# # missing values
-# missing_values = df.isnull().sum()
+# plot - semi 3d mapping between 3 values
+df.plot.scatter(x='citric acid',y='alcohol',c='quality',cmap='coolwarm')
+plt.show()
 
-# print(missing_values)
+# console - testing out sorting
+print("Descending Sorting based on quality")
+df.sort_values("quality", axis = 0, ascending = False,
+                 inplace = True, na_position ='last')
 
-# quality_counts = df['quality'].value_counts()
+print(df.head())
 
-# # Create a bar chart
-# plt.bar(quality_counts.index, quality_counts.values)
+# console - missing values
+print("\nMissing value : ")
+missing_values = df.isnull().sum()
+print(missing_values)
 
-# # Set the labels and title
-# plt.xlabel('Quality')
-# plt.ylabel('Count')
-# plt.title('Wine Quality Distribution')
+#plot - quality bar
+quality_counts = df['quality'].value_counts()
 
-# # Display the chart
-# plt.show()
+# Create a bar chart
+plt.bar(quality_counts.index, quality_counts.values)
+
+# Set the labels and title
+plt.xlabel('Quality')
+plt.ylabel('Count')
+plt.title('Wine Quality Distribution')
+
+# Display the chart
+plt.show()
+
+# Create a pie plot
+plt.pie(quality_counts, labels=quality_counts.index, autopct='%1.1f%%')
+plt.title('Distribution of Quality')
+plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle
+
+# Display the plot
+plt.show()
+
+# Extract the 'alcohol' column
+alcohol_column = df['alcohol']
+
+# Define the bins for alcohol levels
+bins = [0, 9, 10, 11, 12, 13, 14, 15, 16, 17, 20]
+
+# Create a histogram of alcohol levels
+alcohol_counts, _ = np.histogram(alcohol_column, bins=bins)
+
+# Define the labels for the pie plot
+labels = ['0-9', '9-10', '10-11', '11-12', '12-13', '13-14', '14-15', '15-16', '16-17', '17-20']
+
+# Create a pie plot
+plt.pie(alcohol_counts, labels=labels, autopct='%1.1f%%')
+plt.title('Distribution of Alcohol Levels')
+plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle
+
+# Display the plot
+plt.show()
